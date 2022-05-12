790ccae84ebd395859fc32a5d8fef359e08bb744
Elyasaflehrer <0548491959l@gmail.com>
2022-05-12 12:51:39 +0300

    delete key_pair

--- module_1/main.tf
+++ module_1/main.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "minion1_le_saltstack" {
 
 resource "aws_key_pair" "keypair" {
   key_name   = "keypair_learning_saltstack"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCSTewr5KrqfQuwSO5I4jCAsFtYMq+9Sxf4kkB+DJk4RutxmhntBOxD4FRE6yoo/lk3KdxL0VPg0dEvovEXSmtyFS3sxUZv+MQvhF7UarPJ5TJCkQeeTWgHWSOMXhM55UGIXgVBoaDSELojxvdYoFLaC63CGA8FRLIifn+fXTli9Ma5K+bsjzOBquXKYAjLxK8UVh397pulqT5Oi5EPRqpfBdQKRAg8aLd5dpzBaMAQOx65kD3AvJHebpYK80nm3bDTQj+HpHdxFw2YbPIvx0AX5JWLEqAqTHALkgkqjyR4epWJMADjmPdNumHgqQPoaW9OivXsEq70hXlAXl07obF6dvvSVZPiT2JSe64Jc7zmcEc57t9Ze5dLNiIMVVa98UIuG/oreffxsjlAm6mCYvbTCWDz7csC3G0RQz+b/OysmmWlBV9XQGb2+I9TdGIuQ7renLB0tVnUC4TJXHfZ9deyLyxdR9KoquFj+axpVWGkqyI40PNCbcPo4BdKIR0d1b0= ubuntu@ip-172-31-22-109"
+  public_key = ""
 }
 
 
@@ -48,9 +48,3 @@ resource "aws_key_pair" "kp" {
   public_key = tls_private_key.pk.public_key_openssh
 }
 
-public_ip_master = "3.72.61.252"
-public_ip_minion1 = "3.125.40.210"
-public_ip_minion2 = "3.127.36.127"*/
-
-#master.pem:  43:ad:f7:19:81:c4:cd:55:03:d5:44:97:db:7c:ad:f2:83:7b:eb:c5:6d:f9:ac:db:75:ab:d9:53:80:8c:48:cf
-#master.pub:  90:47:c4:03:87:da:b2:49:82:44:97:d4:2e:5c:8d:35:01:8a:e6:a9:49:47:58:7d:5f:09:77:10:34:9c:9b:af

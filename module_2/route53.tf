6d4d75e8623e05df41ad10d35228edd4f72c7da1
Elyasaflehrer <0548491959l@gmail.com>
2022-06-09 13:42:49 +0300

    parted certificate file to route53 file

---
+++ module_2/route53.tf
@@ -0,0 +1,34 @@
+data "aws_route53_zone" "example" {
+  name         = "your_domain_name_from_rout53"
+  private_zone = false
+}
+
+resource "aws_route53_record" "alias_r53_record" {
+  zone_id = data.aws_route53_zone.example.zone_id
+  name    = "www.example.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.aws_lb.dns_name
+    zone_id                = aws_lb.aws_lb.zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "r53_record" {
+  
+  for_each = {
+    for dvo in aws_acm_certificate.acm_tf.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+  
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.example.zone_id
+}

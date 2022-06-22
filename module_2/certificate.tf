3b653402d13d990e7d6b5d455bf157bd71070e75
Elyasaflehrer <0548491959l@gmail.com>
2022-06-22 07:31:05 +0300

    delete empty lines

--- module_2/certificate.tf
+++ module_2/certificate.tf
@@ -4,10 +4,7 @@ resource "aws_acm_certificate" "acm_tf" {
   tags = {
     Name = "certificate_alb_project"
   }
-
 }
-
-
 
 resource "aws_acm_certificate_validation" "acm_cer_vali" {
   certificate_arn         = aws_acm_certificate.acm_tf.arn
